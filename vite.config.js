3fa4756b8c8a2c41b45a9bfb33b8303bcb827c6c
Ian Kipchirchir Koech <iankipchirchir51@gmail.com>
2026-05-05 20:13:43 +0300

    chore(config): fix proxy setup for Deezer API

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Proxy for Deezer API
+      "/api/deezer": {
+        target: "https://api.deezer.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/deezer/, ""),
+      },
+    },
+  },
+});
