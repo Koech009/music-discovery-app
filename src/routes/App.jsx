228eed3f8640f4279a18858ff7f5ce9e260dc6d6
Ian Kipchirchir Koech <iankipchirchir51@gmail.com>
2026-05-05 19:42:56 +0300

    chore(routes): wire up Home, Navbar, Footer

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,10 +1,25 @@
-import "../styles/App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "../components/NavBar.jsx";
+import Footer from "../components/Footer.jsx";
+import Home from "../pages/Home.jsx";
+// import Search from "../pages/Search.jsx";
+// import VideoPage from "../pages/VideoPage.jsx";
+// import FavoritesPage from "../pages/FavoritesPage.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Music Discovery App</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/search" element={<Search />} /> */}
+          {/* <Route path="/video/:artist/:title" element={<VideoPage />} /> */}
+          {/* <Route path="/favorites" element={<FavoritesPage />} /> */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
