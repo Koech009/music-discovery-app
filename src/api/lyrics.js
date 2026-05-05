084c582cc6c93a3f3641dd29d250f5732ee80ba0
Terrunda <terrencegift49@gmail.com>
2026-05-05 18:48:48 +0300

    Created a new export SearchForLyric in lyrics.js to be used in other parts  of the program.
    Created the basic structure for the SearchBar component.

--- src/api/lyrics.js
+++ src/api/lyrics.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
-let artist = 'celeste'
-let title = 'stop ths flame'
-
-axios.get(`https://api.lyrics.ovh/v1/${artist}/${title}`)
-  .then(response => {
-    console.log(response.data); // Data is already parsed as a JS object
-  })
-  .catch(error => {
-    console.error(error.response.data, error.response.status);
-  });
+export function SearchForLyric(artist, title) {
+    axios.get(`https://api.lyrics.ovh/v1/${artist}/${title}`)
+    .then(response => {
+        console.log(response.data);
+    })
+    .catch(error => {
+        console.error(error.response.data, error.response.status);
+    });
+};
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function SearchBar() {
+    const [SearchQuery, setSearchQuery] = useState('')
+// Forms for search queries.
+    return (<form>
+
+    </form>);
+};
+
+export default SearchBar;
