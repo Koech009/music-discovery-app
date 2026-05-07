3960bc8836fdb266f3ce8ab5cb3913586b939ff4
Terrunda <terrencegift49@gmail.com>
2026-05-07 19:00:17 +0300

    First commit.

---
+++ src/__tests__/api.test.js
@@ -0,0 +1,2 @@
+// There is a problem when accessing lyrics that can not be found.
+// After trying to read a song that does not have lyrics, the caption could not display lyrics appears with lyrics underneath it.
