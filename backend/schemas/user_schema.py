a58cc50c33f22b3f21e19b044262d840b20fe41e
Terrunda <terrencegift49@gmail.com>
2026-05-12 22:41:50 +0300

    Adjusted some validations in the UserSchema while adding a new schema: UserPublicSchema; which excludes the password field and its value.

--- backend/schemas/user_schema.py
+++ backend/schemas/user_schema.py
@@ -6,9 +6,14 @@ class UserSchema(SQLAlchemyAutoSchema):
         model = 'User' 
         # Convert user back to object after importing the User model.
         load_instance = True
+        include_relationships = True
 
     # Validations
-    username = fields.Str(validate = validate.Range(3,50))
+    username = fields.Str(validate = validate.Length(min=3,max=50))
     email = fields.Email(required = True) 
-    password = fields.String(required = True, validate = validate.Length(min=6), dump_only = True)
+    password = fields.String(required = True, validate = validate.Length(min=6), load_only = True)
     role = fields.String(validate = validate.OneOf(["user", "admin"]))
+
+class UserPublicSchema(UserSchema):
+    class Meta(UserSchema.Meta):
+        exclude = ("password")
