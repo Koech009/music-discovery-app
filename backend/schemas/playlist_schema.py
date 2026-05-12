260bb29eb4c26510ef77a02a61fd6894e8f866cd
Terrunda <terrencegift49@gmail.com>
2026-05-12 20:48:32 +0300

    Created the template of the playlist schema

--- backend/schemas/playlist_schema.py
+++ backend/schemas/playlist_schema.py
@@ -1,5 +1,14 @@
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, fields, validate
 # Handle importation of the playlist model after here.
 
 class PlaylistSchema(SQLAlchemyAutoSchema):
-    pass
+    class Meta:
+        model = 'Playlist'
+        # Convert user back to object after importing the Playlist model.
+        load_instance = True
+        include_fk = True 
+        # Determines whether foreign key fields are included in the serialized output.
+
+    # Validations
+    name = fields.String(required = True, validate = validate.Range(min =1, max = 100))
+
