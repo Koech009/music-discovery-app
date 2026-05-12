6a9e359cecd1d6e6c74ddafce09f805a3564a1a0
Terrunda <terrencegift49@gmail.com>
2026-05-12 18:47:38 +0300

    Defined the schema for UserSchema.

--- backend/schemas/user_schema.py
+++ backend/schemas/user_schema.py
@@ -1,5 +1,14 @@
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, fields, validate
 # Handle importation of the user model after here.
 
 class UserSchema(SQLAlchemyAutoSchema):
-    pass
+    class Meta:
+        model = 'User' 
+        # Convert user back to object after importing the User model.
+        load_instance = True
+
+    # Validations
+    username = fields.Str(validate = validate.Range(3,50))
+    email = fields.Email(required = True) 
+    password = fields.String(required = True, validate = validate.Length(min=6), dump_only = True)
+    role = fields.String(validate = validate.OneOf(["user", "admin"]))
