7f6c03d8a12f82feef60fb5f5594b60ea5db69bb
Ian Kipchirchir Koech <iankipchirchir51@gmail.com>
2026-05-13 22:08:46 +0300

    refactor(ErrorMessages): simplify conditional rendering and styling

--- src/components/ErrorMessages.jsx
+++ src/components/ErrorMessages.jsx
@@ -1,5 +1,5 @@
 import "../styles/ui.css";
-
+// A simple reusable component that displays error messages in ui and takes a message prop. If the message is empty or null, it returns null and renders nothing.
 function ErrorMessages({ message }) {
   if (!message) return null;
   return <div className="error">{message}</div>;
